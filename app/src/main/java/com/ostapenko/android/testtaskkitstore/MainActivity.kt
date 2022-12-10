b8f677eeda93de97d09c3abb9941e94e00625d37
Elestag <jonesx45@gmail.com>
2022-12-10 18:13:21 +0200

    WebView

--- app/src/main/java/com/ostapenko/android/testtaskkitstore/MainActivity.kt
+++ app/src/main/java/com/ostapenko/android/testtaskkitstore/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.ostapenko.android.testtaskkitstore
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        webView = findViewById(R.id.webview)
+
+        if (webView.canGoBack()){
+            webView.goBack()
+        }else{
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/ostapenko/android/testtaskkitstore/ui/WebViewFragment.kt
+++ app/src/main/java/com/ostapenko/android/testtaskkitstore/ui/WebViewFragment.kt
@@ -1,8 +1,31 @@
 package com.ostapenko.android.testtaskkitstore.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.ostapenko.android.testtaskkitstore.R
 
-class WebViewFragment:Fragment(R.layout.fragment_web_view) {
+class WebViewFragment:Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_web_view, container,false)
+
+        val webView: WebView = view.findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.settings.loadsImagesAutomatically = true
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.google.com/")
+
+        return view
+    }
+
 
 }
